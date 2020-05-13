274ecdcc36f16e71abff9c823634b5d12c412f9e
Anes-Marzooki <marzoukianas@gmail.com>
2020-05-13 21:02:02 +0100

    adds_readme_file

--- meditate_time.py
+++ meditate_time.py
@@ -8,5 +8,5 @@ breaks_count = 0
 print ("The program lanched at :" + time.ctime())
 while (breaks_count < daily_breaks):
     time.sleep(0.5)
-    webbrowser.open('https://www.youtube.com/watch?v=ARt9HV9T0w8')
+    webbrowser.open('https://www.youtube.com/watch?v=iebciuBXCh4')
     breaks_count = breaks_count + 1
---
+++ readme.md
@@ -0,0 +1,9 @@
+### Simple exercices made in python 3.8
+
+#### Meditation Time
+
+Opens the webbrowser after a fixed amount of time has passed and plays a soothing video.
+
+#### Rename Files
+
+Get rid of numbers in a files' names.
